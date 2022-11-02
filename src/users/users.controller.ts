14172071589936011281014efc4a1ec85fd3f6b1
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2022-11-02 22:52:20 +0200

    feat: create User entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import { KEYS } from './../keys';
 import type { ILogger } from './../logger/logger.interface';
 import type { UserLoginDTO } from './dto/user-login.dto';
 import type { UserRegisterDTO } from './dto/user-register.dto';
+import { User } from './user.entity';
 import type { IUsersController } from './users.controller.interface';
 
 @injectable()
@@ -28,13 +29,17 @@ export class UsersController extends BaseController implements IUsersController 
     ]);
   }
 
-  register(req: Request<{}, {}, UserRegisterDTO>, res: Response, next: NextFunction): void {
-    console.log(req.body);
-    this.created(res, { message: 'Registered' });
+  async register(
+    { body }: Request<{}, {}, UserRegisterDTO>,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const newUser = new User(body.email, body.name);
+    await newUser.setPassword(body.password);
+    this.created(res, { email: newUser.email, name: newUser.name });
   }
 
   login(req: Request<{}, {}, UserLoginDTO>, res: Response, next: NextFunction): void {
-    console.log(req.body);
     this.ok(res, { message: 'Logged in' });
   }
 }
