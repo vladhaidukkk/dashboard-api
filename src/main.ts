22355bd4a7021be1e49120d5f43595791faeceda
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2022-11-02 14:06:51 +0200

    feat: add routing to basic http Node.js server

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello, world!</h1>');
+  switch (req.method) {
+    case 'GET':
+      switch (req.url) {
+        case '/':
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          res.end('<h1>Hello, world!</h1>');
+          break;
+      }
+      break;
+  }
 });
 
 server.listen(port, hostname, () => {
